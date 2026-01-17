4b340829f15abecb1119a06fec01a79c146884bc
kiokomutunga <kiokomutungapp@gmail.com>
2026-01-18 00:11:52 +0300

    send email util controller

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,11 +1,27 @@
-const nodemailer = require("nodemailer");
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS }
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
-exports.sendEmail = ({ to, subject, text, html }) =>
-  transporter.sendMail({ from: process.env.EMAIL_USER, to, subject, text, html });
+// Named export (matches your import)
+export const sendEmail = ({ to, subject, text, html }) => {
+  return transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+    html,
+  });
+};
+
+// Optional debug logs (safe)
 console.log("Email user:", process.env.EMAIL_USER);
-console.log("Email pass:", process.env.EMAIL_PASS ? "Loaded" : "Not Loaded");
+console.log(
+  "Email pass:",
+  process.env.EMAIL_PASS ? "Loaded" : "Not Loaded"
+);
