0c4807092b24b9fb09f5b874cc413ef0dd9ca3c1
kiokomutunga <kiokomutungapp@gmail.com>
2026-01-18 00:18:29 +0300

    changing to es model

--- models/User.js
+++ models/User.js
@@ -1,13 +1,17 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  phone: String,
-  password: String,
-  isVerified: { type: Boolean, default: false },
-  role: { type: String, enum: ["user", "admin"], default: "user" }
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: { type: String, unique: true },
+    phone: String,
+    password: String,
+    isVerified: { type: Boolean, default: false },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+  },
+  { timestamps: true }
+);
 
+const User = mongoose.model("User", userSchema);
 
-export default mongoose.model("User", userSchema);
+export default User;
