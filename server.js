f5ce6ab0d8701d6a8f25e1d09f875d9f4bdf1061
kiokomutunga <kiokomutungapp@gmail.com>
2026-01-20 22:22:29 +0300

    app route use

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Routes
 app.use("/api/invoices", invoiceRoutes);
-app.use("api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(` Server running on https://invoice-backend-repository-scaffold.onrender.com:${PORT}`));
